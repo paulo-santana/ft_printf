ab49a8340e3c10d48cc6f9d20e9570fdf2aeea8d
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-03 06:26:38 -0300

    fix(intd): printing negative numbers with precision

--- src/handlers_intd.c
+++ src/handlers_intd.c
@@ -17,22 +17,25 @@ static int	fill_precision(t_param *param)
 	int		i;
 	int		str_len;
 	char	*new_str;
+	int		is_neg;
 
+	is_neg = param->str[0] == '-';
 	str_len = ft_strlen(param->str);
 	if (str_len >= param->precision)
 		return (str_len);
-	new_str = malloc(param->precision + 1);
+	new_str = malloc(param->precision + is_neg + 1);
 	if (new_str == NULL)
 		return (-1);
 	i = 0;
-	new_str[param->precision] = '\0';
-	while (i < param->precision - str_len)
+	if (is_neg)
+		new_str[i++] = '-';
+	while (i < param->precision - str_len + is_neg)
 		new_str[i++] = '0';
-	new_str[i] = '\0';
-	ft_strlcat(new_str, param->str, param->precision + 1);
+	new_str[i + is_neg] = '\0';
+	ft_strlcat(new_str, param->str + is_neg, param->precision + 1 + is_neg);
 	free(param->str);
 	param->str = new_str;
-	param->str_len = param->precision;
+	param->str_len = param->precision + is_neg;
 	return (param->precision);
 }
 
